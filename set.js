e9a769d17ac5f4bc559172a0a3c77f21be9fecde
yuki <yuki77x@googlemail.com>
2014-05-04 18:36:39 +0200

    04//05
    merged -side.js and -top.js to sim.js
    removed .gitignore
    added Themisto
    added "observer position"

--- set.js
+++ set.js
@@ -1,16 +1,17 @@
 function set(){
 	var t = document.getElementById("ut").value;
 	var ts = document.getElementById("uts").value;
+	var vs = document.getElementById("uvs").value;
+	var mode = document.getElementById("umode").value;
 	
-	if(t==""){
-		t = 0;
+	if(mode>=4){
+		window.alert("invalid mode, please choose an integer between 1 and 3");
+	}else{
+		localStorage.setItem("t", t);
+		localStorage.setItem("ts", ts);
+		localStorage.setItem("vs", vs);
+		localStorage.setItem("mode", mode);
+		
+		location.reload(); 
 	}
-	if(ts==""){
-		ts = 5;
-	}
-	
-	localStorage.setItem("t", t);
-	localStorage.setItem("ts", ts);
-	
-	location.reload(); 
 }
